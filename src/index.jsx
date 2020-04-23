2570137b60bcd7c26497a08cebac49a67b4a3c98
dan7sc <su7cheng@gmail.com>
2020-04-23 12:01:55 -0300

    Add solution to exercise 2.5: separate module

--- src/index.jsx
+++ src/index.jsx
@@ -1,60 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => {
-    const {course} = props
-
-    return <h1>{course}</h1>
-}
-
-const Part = (props) => {
-    const {name, exercises} = props
-
-    return (
-        <p>
-          {name} {exercises}
-        </p>
-    )
-}
-
-const Content = (props) => {
-    const {parts} = props
-
-    return (
-        <div>
-          {parts.map(part => <Part
-                               key={part.id}
-                               name={part.name}
-                               exercises={part.exercises}
-                             />
-                    )
-          }
-        </div>
-    )
-}
-
-const Total = (props) => {
-    const {parts} = props
-    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-    return (
-        <div>
-          <b>total of {total} exercises</b>
-        </div>
-    )
-}
-
-const Course = (props) => {
-    const {course} = props
-
-    return (
-        <div>
-          <Header course={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
-        </div>
-    )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
